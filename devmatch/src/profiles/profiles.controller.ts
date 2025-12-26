c1cdc98b9162b17dfe1c512a6363b39a51d2b07d
AsokTamang <ashoktmg2058@gmail.com>
2025-12-26 12:27:45 -0500

    learned using query in controller of nest

--- devmatch/src/profiles/profiles.controller.ts
+++ devmatch/src/profiles/profiles.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 
 @Controller('profiles') //this is our route which is like this /profiles
 export class ProfilesController {
-  @Get()   //this is our get route
-  findAll() {
-    return [];
+  @Get() //this is our get route
+  findAll(@Query('location') location: string) {
+    //as in the req url the query is always in string, so we must pass the string location inside query decorator then we are also settign the type of location as we are using ts
+    return [{ location }];
   }
 }
