117029f982e7a031e9ba8028060cd2844ebca62b
AsokTamang <ashoktmg2058@gmail.com>
2025-12-26 11:37:46 -0500

    installation of nestjs

---
+++ devmatch/src/main.ts
@@ -0,0 +1,8 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule,{abortOnError:false}); //this abortOnError property throws the actual error instead of exiting the code when any error occures in our app
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
