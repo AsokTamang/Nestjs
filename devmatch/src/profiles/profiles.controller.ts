7b0785ea5ff75fc3bd6901e8cf1b9678067ce416
AsokTamang <ashoktmg2058@gmail.com>
2025-12-27 20:11:41 -0500

    bubbling up from service to controller

--- devmatch/src/profiles/profiles.controller.ts
+++ devmatch/src/profiles/profiles.controller.ts
@@ -4,51 +4,40 @@ import {
   Get,
   Param,
   Post,
-  Query,
   Put,
   Delete,
   HttpCode,
   HttpStatus,
 } from '@nestjs/common';
-import { CreateProfileDto } from './dto/create-profile.dto';
 import { UpdateProfileDto } from './dto/update-profile.dto';
 import { ProfilesService } from './profiles.service';
-
+import { CreateProfileDto } from './dto/create-profile.dto';
 @Controller('profiles') //this is our route which is like this /profiles
 export class ProfilesController {
-  constructor(private ProfilesService: ProfilesService) {}
+  constructor(private ProfilesService: ProfilesService) {}  //here as the data variable in profileservice is private, we are using constructor inorder to access this private data var
   @Get() //this is our get route
   findAll() {
     //as in the req url the query is always in string, so we must pass the string location inside query decorator then we are also settign the type of location as we are using ts
-    return this.ProfilesService;
+    return this.ProfilesService.findALL();
   }
   @Get(':id') //as the id is passed in url as param as :id
   findOne(@Param('id') id: String) {
-    return this.ProfilesService.findOne(id);
+     return this.ProfilesService.findOne(id);
   }
   @Post() //here we are implementing the post method
-  create(@Body() CreateProfileDto: CreateProfileDto) {
+  create(@Body() body: CreateProfileDto) {
     //here we are assigning that the name and the description passed from the req body must be like the name and description as in dto
     //if any bad req is made then nest automatically throws error
-    return [
-      {
-        name: CreateProfileDto.name,
-        description: CreateProfileDto.description,
-      },
-    ];
+    return this.ProfilesService.create(body);
   }
-  @Put(':id')
+  @Put(':id') //updating the profile based on id
   update(@Param('id') id: string, @Body() UpdateProfileDto: UpdateProfileDto) {
-    return {
-      id,
-      name: UpdateProfileDto.name,
-      description: UpdateProfileDto.description,
-    };
+    return this.ProfilesService.updateProfile(id, UpdateProfileDto);
   }
   @Delete(':id')
-  @HttpCode(HttpStatus.NO_CONTENT)
-  remove(@Param('id') id: string) {
+  @HttpCode(HttpStatus.NO_CONTENT)   
+  remove(@Param('id') id: string):void {
     //here we are using Delete method with function remove
-    return {};
+    return this.ProfilesService.deleteOne(id);
   }
 }
--- devmatch/src/profiles/profiles.service.ts
+++ devmatch/src/profiles/profiles.service.ts
@@ -1,30 +1,61 @@
-import { Injectable } from '@nestjs/common';
+//the profile service consists of all the logics
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { randomUUID } from 'crypto';
+import { CreateProfileDto } from './dto/create-profile.dto';
+import { UpdateProfileDto } from './dto/update-profile.dto';
 
 @Injectable()
 export class ProfilesService {
-    private profiles = [
-        {
+  private profiles = [
+    {
       id: randomUUID(),
       name: 'Brianna Watts',
-      description: `Looking for someone to merge with my heart. I’m a full-stack romantic who refactors my feelings until they pass all tests. Bonus points if you can debug my issues while we pair program over coffee. Let’s commit to something beautiful together.`
+      description: `Looking for someone to merge with my heart. I’m a full-stack romantic who refactors my feelings until they pass all tests. Bonus points if you can debug my issues while we pair program over coffee. Let’s commit to something beautiful together.`,
     },
     {
       id: randomUUID(),
       name: 'Jasper Quinn',
-      description: `Seeking a partner in crime to compile my heart. Must be comfortable with the terminal because I only speak fluent bash. Swipe right if you can appreciate a good kernel panic every now and then.`
+      description: `Seeking a partner in crime to compile my heart. Must be comfortable with the terminal because I only speak fluent bash. Swipe right if you can appreciate a good kernel panic every now and then.`,
     },
     {
       id: randomUUID(),
       name: 'Leo Park',
-      description: `You think you know VIM? Try Neovim. I'll make your modal dreams come true. Want to escape the matrix and explore the perfect keyboard shortcut for love?`
+      description: `You think you know VIM? Try Neovim. I'll make your modal dreams come true. Want to escape the matrix and explore the perfect keyboard shortcut for love?`,
+    },
+  ];
+  findALL() {
+    return this.profiles; //we can return the private data only using this.
+  }
+  findOne(id: String) {
+    const existing  = this.profiles.find((profile) => profile.id === id);
+    if (existing){
+      return existing
     }
-    ]
-    findALL(){
-        return this.profiles   //we can return the private data only using this.
+    throw new NotFoundException(); 
+  }
+  create(body: CreateProfileDto) {
+    //this req comes from the controller
+    const { name, description } = body;
+    const newData = { id: randomUUID(), name, description };
+    this.profiles.push(newData);
+    return newData;
+  }
+  updateProfile(id: string, body: UpdateProfileDto) {
+   const profile = this.findOne(id);
+   if (!profile){
+    throw new NotFoundException('Profile not found!');
+   }
+   Object.assign(profile,body)  //here we are updating the info using id and body 
+   
+    return profile;
+  }
+  deleteOne(id:string){
+    const profileIndex = this.profiles.findIndex((profile)=>profile.id === id);
+    if (profileIndex>-1){   //if there is no any element having given provided id then the findIndex method returns -1, which is why we are comparing with -1 here
+     this.profiles.splice(profileIndex,1)  //here we are removing one item from the given profileIndex from the given array
+     return
     }
-    findOne(id:String){
-        return this.profiles.find((profile)=>profile.id===id);
-
-    }
+    return
+   
+  }
 }
