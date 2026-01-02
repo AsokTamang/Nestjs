3ff4963f7b02db03fbcdb6de46463165421d77a3
AsokTamang <ashoktmg2058@gmail.com>
2026-01-02 12:20:19 -0500

    relation between group and users done

--- backend/models/groups.enitity.ts
+++ backend/models/groups.enitity.ts
@@ -1,14 +1,20 @@
-import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  ManyToMany,
+  JoinTable,
+} from 'typeorm';
 import { User } from './profile.entity';
-import { UserType } from 'types';
+
 @Entity()
 export class Group {
-  @PrimaryGeneratedColumn()  //primary key
+  @PrimaryGeneratedColumn() //primary key
   id: number;
   @Column()
-  name: string;  //name of the group
- 
- @OneToMany((type:UserType) => User, (User:UserType) => User.id)
-  members:User[]   //here we are declaring that this model called Group has relation with many users and these users are stored inside an array called members
+  name: string; //name of the group
 
+  @ManyToMany(() => User, (User) => User.groups)
+  @JoinTable()
+  members: User[]; //here we are declaring that this model called Group has relation with many users and these users are stored inside an array called members
 }
--- backend/models/profile.entity.ts
+++ backend/models/profile.entity.ts
@@ -1,4 +1,5 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn,ManyToMany } from 'typeorm';
+import { Group } from './groups.enitity';
 
 @Entity()
 export class User {
@@ -16,4 +17,10 @@ export class User {
 
   @Column()
   password: string;
+  
+ 
+  @ManyToMany(() => Group, (Group) => Group.members)
+  groups: Group[]; //
+
+
 }
